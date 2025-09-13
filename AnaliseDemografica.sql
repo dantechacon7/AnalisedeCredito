e04c6b2075546b1fc7fdd082fe750e352b2f03aa
Dante Chacon <dantec.chacon@gmail.com>
2025-09-13 15:11:13 -0300

    Update AnaliseDemografica.sql

--- AnaliseDemografica.sql
+++ AnaliseDemografica.sql
@@ -8,44 +8,29 @@ FROM credito_experimento;
   Primeiro, devemos contabilizar quantas pessoas são do gênero feminino, e quantas pessoas são do gênero masculino. */
 
 SELECT 
-    gender,
-    COUNT(*) AS count
+    COUNT(DISTINCT id) as volume_pessoas,
+    CASE WHEN gender = 0 THEN 'Masculino'
+    WHEN gender = 1 THEN 'Feminino'
+    ELSE null END AS gender
 FROM 
     credito_experimento
-WHERE 
-    gender IN (0, 1)
 GROUP BY 
     gender;
---Resultado: Gender 1 = 3386; Gender 0 = 6323
+--Resultado: Gender Feminino = 3386; Gender Masculino = 6323
 
 /* Agora, calculamos as proporções em relação ao total de pessoas do dataset. 
-  Para isso, vamos criar duas subqueries, uma (gender_counts) que contabiliza indivíduos por gênero, como foi feito na query acima.
-  Outra subquery que levanta o total de indivíduos do dataset. E, por fim, uma query final que determina a proporção (proportion) */
-
-WITH gender_counts AS (
-    SELECT 
-        gender,
-        COUNT(*) AS count
-    FROM 
-        credito_experimento
-    WHERE 
-        gender IN (0, 1)
-    GROUP BY 
-        gender
-), total_count AS (
-    SELECT 
-        COUNT(*) AS total
-    FROM 
-        credito_experimento
-    WHERE 
-        gender IN (0, 1)
+  Para isso, vamos contabilizar indivíduos por gênero, como foi feito na query acima, e levantar o total de indivíduos do dataset. 
+E, por fim, uma seleção final que determina a proporção (proportion) de cada caso */
+WITH calculos as (
+  SELECT
+  COUNT(DISTINCT CASE WHEN gender = 1 THEN id END) AS volume_mulheres,
+  COUNT(DISTINCT CASE WHEN gender = 1 THEN id END) AS volume_homens,
+  COUNT(DISTINCT id) AS volume_total
+  FROM credito_experimento
 )
 SELECT 
-    gc.gender,
-    gc.count,
-    gc.count * 1.0 / tc.total AS proportion
-FROM 
-    gender_counts gc,
-    total_count tc;
+format('%.1f%%', (volume_mulheres * 100.0) / volume_total) AS proporcao_feminina --proporção formatada com %
+format('%.1f%%', (volume_homens * 100.0) / volume_total) AS proporcao_masculina
+FROM calculos
 
 /* Resultado: Gender 1 possui 3386 registros, numa proporção de 34.87% do dataset. Gender 0 possui 6323 registros, numa proporção de 65.12% do dataset./*
